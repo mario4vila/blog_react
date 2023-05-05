1fe714ed05d479910d80be00bfb1768ee8163b7e
mario4vila <marioo.rab@gmail.com>
2023-05-05 13:45:13 +0200

    understanding jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './Greeting';
+
+function hello(name, age){
+  return (
+    <div>
+      <h2>Soy {name}</h2>
+      <h2>Tengo {age} años</h2>
+    </div>
+  );
+}
+
 
 function App() {
+  const name = 'Mario';
+  const age = 12
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {hello(name, age)}
         </p>
         <a
           className="App-link"
@@ -17,6 +33,7 @@ function App() {
         >
           Learn React
         </a>
+        <Greeting/>
       </header>
     </div>
   );
---
+++ src/Greeting.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Greeting = () => {
+    const name = 'lobita';
+
+  return (
+    <div>
+        <h1>Este saludo es para</h1>
+        <p>{name}</p>
+    </div>
+    
+  )
+}
+
+export default Greeting
