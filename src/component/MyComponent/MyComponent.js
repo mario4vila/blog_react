8eb8411624f0cc25951b6dd08590655dcf7b98bf
mario4vila <marioo.rab@gmail.com>
2023-05-05 17:32:29 +0200

    using loops in components

--- src/component/MyComponent/MyComponent.js
+++ src/component/MyComponent/MyComponent.js
@@ -1,11 +1,32 @@
 import React from 'react'
 
 class MyComponent extends React.Component {
-
     render(){
+        const recipe = {
+            name: 'Pizza',
+            calories: 400,
+            ingredients: [
+                'tomato',
+                'cheese',
+                'cooked ham'
+            ]
+        }
+
         return (
             <div>
-                <h1>Hello, I am my component</h1>
+                <h1>Recipe {recipe.name}</h1>
+                <h2>Calories {recipe.calories}</h2>
+                <ol>
+                    {
+                        recipe.ingredients.map((ingredient, i)=>{
+                            return (
+                                <li key={i}>
+                                    {ingredient}
+                                </li>
+                            );
+                        })
+                    }
+                </ol>
             </div>
         );
     }
