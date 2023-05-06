d315c31f59c53ffe47430e76f0e7366ae6233e59
mario4vila <marioo.rab@gmail.com>
2023-05-06 12:50:00 +0200

    crete Sidebar component

--- src/App.js
+++ src/App.js
@@ -4,48 +4,61 @@ import MyComponent from './component/MyComponent/MyComponent';
 import Movies from './component/Movies/Movies';
 import Header from "./component/Header";
 import Slider from "./component/Slider";
+import Sidebar from "./component/Sidebar";
 
-function hello(name, age){
-  return (
-    <div>
-      <h2>Soy {name}</h2>
-      <h2>Tengo {age} años</h2>
-    </div>
-  );
+function hello(name, age) {
+    return (
+        <div>
+            <h2>Soy {name}</h2>
+            <h2>Tengo {age} años</h2>
+        </div>
+    );
 }
 
 
 function App() {
-  const name = 'Mario';
-  const age = 12
-  return (
-    <div className="App">
-      <Header/>
-      <Slider/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          {hello(name, age)}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <section className='components'>
-        <MyComponent/>
-        <Movies/>
-        </section>
+    const name = 'Mario';
+    const age = 12
+    return (
+        <div className="App">
+            <Header/>
+            <Slider/>
 
-      </header>
-    </div>
-  );
+            <div className="center">
+                <section id="content">
+                    <h2 className="subheader">Últimos artículos</h2>
+
+                </section>
+
+                <Sidebar/>
+
+                <div className="clearfix"></div>
+            </div>
+
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <p>
+                    {hello(name, age)}
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <section className='components'>
+                    <MyComponent/>
+                    <Movies/>
+                </section>
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
