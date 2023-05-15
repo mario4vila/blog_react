92a2fd1ea2d0805e428e15c230e7e644a5270d0a
mario4vila <marioo.rab@gmail.com>
2023-05-15 14:16:19 +0200

    Using ref properties in form

--- src/page/Form.js
+++ src/page/Form.js
@@ -3,6 +3,19 @@ import Slider from "../component/Slider";
 import Sidebar from "../component/Sidebar";
 
 class Form extends React.Component {
+
+    refName = React.createRef();
+    refSurname = React.createRef();
+    refBio = React.createRef();
+    refSex = React.createRef();
+
+    submitForm = (e)=>{
+        e.preventDefault();
+        alert(`
+            Form has been sent,
+            Name: ${this.refName.current.value}
+        `)
+    }
     render() {
         return (
             <div id="form">
@@ -12,7 +25,34 @@ class Form extends React.Component {
                 />
                 <div className="center">
                     <section id="content">
-                        <p>Here to be showed form</p>
+                        <h1 className="subheader">Formulario</h1>
+
+                        <form className="mid-form" onSubmit={this.submitForm}>
+                            <div className="form-group">
+                                <label htmlFor="nombre">Nombre</label>
+                                <input type="text" name="nombre" ref={this.refName}/>
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="apellidos">Apellidos</label>
+                                <input type="text" name="apellidos" ref={this.refSurname}/>
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="bio">Biografia</label>
+                                <textarea name="bio" ref={this.refBio}></textarea>
+                            </div>
+
+                            <div className="form-group radibuttons">
+                                <input type="radio" name="genero" value={'hombre'} ref={this.refSex}/> Hombre
+                                <input type="radio" name="genero" value={'mujer'} ref={this.refSex}/> Mujer
+                            </div>
+
+                            <div className="clearfix"></div>
+
+                            <input type="submit" value="Enviar" className="btn btn-success"/>
+
+                        </form>
                     </section>
                     <Sidebar/>
                     <div className="clearfix"></div>
