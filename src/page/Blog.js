673a42e1f77402397813a4f2196277080662706a
mario4vila <marioo.rab@gmail.com>
2023-05-15 18:57:47 +0200

    install and test axios

--- src/page/Blog.js
+++ src/page/Blog.js
@@ -1,8 +1,35 @@
 import React from "react";
 import Slider from "../component/Slider";
 import Sidebar from "../component/Sidebar";
+import axios from 'axios';
 
 class Blog extends React.Component {
+
+    state = {
+        articles: {},
+        status: 'error'
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:3900/api/articles')
+            .then(response => {
+                // handle success
+                console.log(response.data.articles);
+
+                this.setState({
+                    articles: response.data.articles,
+                    status: 'success'
+                });
+            })
+            .catch(function (error) {
+                // handle error
+                // console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
+    }
+
     render() {
         return (
             <div id="blog">
@@ -12,7 +39,15 @@ class Blog extends React.Component {
                 />
                 <div className="center">
                     <section id="content">
-                        <p>Here to be showed articles</p>
+                        {this.state.status === 'success' &&
+                            <div>
+                                {this.state.articles.map((article) => {
+                                    return (
+                                        <p>{article.title}</p>
+                                    );
+                                })}
+                            </div>
+                        }
                     </section>
                     <Sidebar
                         blog={true}
